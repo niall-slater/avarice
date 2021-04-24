77cf0ab938570f94ab529cc426d849c7ddbce187
niall-slater <niallslater@gmail.com>
2021-04-24 11:55:52 +0100

    Add camera scroll

--- Assets/Scripts/UI/CameraController.cs
+++ Assets/Scripts/UI/CameraController.cs
@@ -15,8 +15,13 @@ public class CameraController : MonoBehaviour
     {
         var x = Input.GetAxis("Horizontal");
         var y = Input.GetAxis("Vertical");
+        var scroll = Input.mouseScrollDelta.y;
 
         var movement = new Vector3(x, y, 0f);
+        Camera.main.orthographicSize -= scroll;
+
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, 1f, 12f);
+
         transform.position = Vector3.Lerp(transform.position, transform.position + movement, Time.deltaTime * 10f);
     }
 }
